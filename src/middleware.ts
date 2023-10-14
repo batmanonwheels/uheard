91d4556a2f1a646b169201903301b0c99f0cdae5
Devin Garcia <devingarcia5423@gmail.com>
2023-10-14 11:16:47 -0400

    fix: user unable  to browse tracks w/ expd. cookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,10 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-	if (!request.cookies.get('spotify_oauth_state'))
-		return NextResponse.redirect(new URL('/', request.url));
+	if (!request.cookies.get('spotify_oauth_state')) {
+		console.log('hi');
+		return NextResponse.redirect(new URL('/api/login/spotify', request.url));
+	}
 }
 
 export const config = {
