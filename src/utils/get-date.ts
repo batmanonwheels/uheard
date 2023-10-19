53e66b3ed7b742d41bf67a9a2e31e53b5a968260
Devin Garcia <devingarcia5423@gmail.com>
2023-10-19 16:08:19 -0400

    update get date function to include precision

--- src/utils/get-date.ts
+++ src/utils/get-date.ts
@@ -14,6 +14,12 @@ export const getDate = async (timestamp: string, precision: string) => {
 		'December',
 	];
 	if (precision === 'year') return `Released in ${timestamp}`;
+
+	if (precision === 'month')
+		return `Released in ${months[parseInt(timestamp.split('-')[1]) - 1]} ${
+			timestamp.split('-')[0]
+		}`;
+
 	return `Released on ${
 		months[parseInt(timestamp.split('-')[1]) - 1]
 	} ${parseInt(timestamp.split('-')[2])}, ${timestamp.split('-')[0]}`;
