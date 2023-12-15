597dc49446560310aaaf18239a3e5dbccd7a9939
Devin Garcia <devingarcia5423@gmail.com>
2023-12-15 13:22:53 -0500

    update next version and add vercel cron job

---
+++ src/app/api/cron/keep-server-alive.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { fetchRecommendations } from '@/utils/fetch-recommendations';
+
+export const GET = async (req: NextRequest, res: NextResponse) => {
+	try {
+		const recommendations = await fetchRecommendations();
+		return NextResponse.json({ recommendations, ok: true });
+	} catch (error: any) {
+		return new NextResponse(error.message, { status: error.status });
+	}
+};
