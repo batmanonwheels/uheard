a8f154a162dff1c41090868300fd069ebd32744c
Devin Garcia <devingarcia5423@gmail.com>
2024-01-13 03:30:14 -0500

    fix cron job

--- src/app/api/cron/keep-server-alive.ts
+++ src/app/api/cron/keep-server-alive.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { fetchRecommendations } from '@/utils/fetch-track-recommendations';
+import { fetchTrackRecommendations } from '@/utils/fetch-track-recommendations';
 
 export const GET = async (req: NextRequest, res: NextResponse) => {
 	try {
-		const recommendations = await fetchRecommendations();
+		const recommendations = await fetchTrackRecommendations();
 		return NextResponse.json({ recommendations, ok: true });
 	} catch (error: any) {
 		return new NextResponse(error.message, { status: error.status });
